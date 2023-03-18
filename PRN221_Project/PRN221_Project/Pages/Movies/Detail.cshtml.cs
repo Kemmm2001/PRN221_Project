a2e7055d9c5f63c7257036b3b274ee38709ce422
luutunglam2001 <luutunglam2001@gmail.com>
2023-03-18 18:22:17 +0700

    detail

--- PRN221_Project/PRN221_Project/Pages/Movies/Detail.cshtml.cs
+++ PRN221_Project/PRN221_Project/Pages/Movies/Detail.cshtml.cs
@@ -17,6 +17,8 @@ namespace PRN221_Project.Pages.Movies
         public Movie movie { get; set; }
         [BindProperty]
         public List<User> users { get; set; }
+        [BindProperty]
+        public User user { get; set; }
         public IActionResult OnGet(int? id)
         {
             if (id != null)
@@ -40,6 +42,24 @@ namespace PRN221_Project.Pages.Movies
             _db.SaveChanges();
             movie =  _db.Movies.Include(p => p.Cate).SingleOrDefault(p => p.MovieId == movie.MovieId);
             ViewData["CategoryId"] = new SelectList(_db.Categories, "CateId", "CateName");
+            users = _db.Users.Include(u => u.Seat).Where(u => u.MovieId == movie.MovieId).ToList();
+            return Page();
+        }
+
+        public IActionResult OnPostDelete(int? id)
+        {
+            Console.WriteLine(id);
+            if (id != null)
+            {
+                user = _db.Users.SingleOrDefault(p => p.UserId == id);
+                var movie = _db.Movies.SingleOrDefault(p => p.MovieId == user.MovieId);
+                movie.Quantity = movie.Quantity - 1;
+                _db.Users.Remove(user);
+                _db.SaveChanges();
+            }
+            movie =  _db.Movies.Include(p => p.Cate).SingleOrDefault(p => p.MovieId == user.MovieId);
+            ViewData["CategoryId"] = new SelectList(_db.Categories, "CateId", "CateName");
+            users = _db.Users.Include(u => u.Seat).Where(u => u.MovieId == user.MovieId).ToList();
             return Page();
         }
     }
